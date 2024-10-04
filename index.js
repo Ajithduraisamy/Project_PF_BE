d94362775dd13dd96e3fc11047a0461002bbf369
Ajithduraisamy <ajithduraisamy.r@gmail.com>
2024-10-04 08:39:43 +0530

    Origin Link Update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: 'https://ajithduraisamy.netlify.app'
 }));
 
 const URL = process.env.URL;
